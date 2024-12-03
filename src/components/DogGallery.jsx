f0c284afc5baf6cf8fb6c3adcb947b589a8cef77
Alex <megaoh478@gmail.com>
2024-12-03 17:42:34 +0200

    ex8test2

--- src/components/DogGallery.jsx
+++ src/components/DogGallery.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { fetchDogImages } from "./api";
+import { fetchDogImages } from "../api";
 import DogCard from "./DogCard";
 import "./DogGallery.css";
 
